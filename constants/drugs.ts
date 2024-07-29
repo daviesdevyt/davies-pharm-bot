41ed52bd78f8597ee25fe4af8931f182c55a89bd
Temidire <temidireowoeye@gmail.com>
2024-07-29 23:24:02 +0100

    Feat: add constants for storing drug details

---
+++ constants/drugs.ts
@@ -0,0 +1,20 @@
+const pain = [
+    { image: "/assets/images/germoloids.svg", name: "Germoloids", currentPrice: 5500, formerPrice: 8000, discount: 22, id: 1 },
+    { image: "/assets/images/tylenol.svg", name: "Tylenol", currentPrice: 17500, id: 2 },
+    { image: "/assets/images/strepsils.svg", name: "Strepsils", currentPrice: 3500, id: 3 },
+    { image: "/assets/images/deep-heat.svg", name: "Deep Heat Rub", currentPrice: 9300, formerPrice: 12000, discount: 12, id: 4 },
+];
+const cold = [
+    { image: "/assets/images/deep-heat.svg", name: "Deep Heat Rub", currentPrice: 9300, formerPrice: 12000, discount: 12, id: 4 },
+    // { image: "/assets/images/germoloids.svg", name: "Germoloids", currentPrice: 5500, formerPrice: 8000, discount: 22 },
+    { image: "/assets/images/strepsils.svg", name: "Strepsils", currentPrice: 3500, id: 3 },
+    // { image: "/assets/images/tylenol.svg", name: "Tylenol", currentPrice: 17500 },
+];
+const vitamins = [
+    { image: "/assets/images/tylenol.svg", name: "Tylenol", currentPrice: 17500, id: 2 },
+    // { image: "/assets/images/deep-heat.svg", name: "Deep Heat Rub", currentPrice: 9300, formerPrice: 12000, discount: 12 },
+    { image: "/assets/images/germoloids.svg", name: "Germoloids", currentPrice: 5500, formerPrice: 8000, discount: 22, id: 1 },
+    // { image: "/assets/images/strepsils.svg", name: "Strepsils", currentPrice: 3500 },
+];
+
+export { pain, cold, vitamins };
