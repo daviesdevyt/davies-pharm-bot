1f6c1be95ecc5eb177c1e028e416fb48c2b0f495
Temidire <temidireowoeye@gmail.com>
2024-08-01 16:39:22 +0100

    fix: add counter to checkout button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import CoughAndCold from "@/components/CoughAndCold";
 import PainManagement from "@/components/PainManagement";
 import Vitamins from "@/components/Vitamins";
+import { useProductsStore } from "@/store/useProducts";
 import Link from "next/link";
 import { useState } from "react";
 
 export default function Home() {
   const [active, setActive] = useState<string>("Pain management");
+  const {product} = useProductsStore();
 
   return (
-    <main className="min-h-screen p-5 relative">
+    <main className="min-h-screen p-5 relative flex flex-col">
       <div className="max-w-8xl m-auto">
         <h3 className="my-5 text-center text-[17px] font-bold text-white py-4">
           PLUGXI
@@ -33,10 +35,15 @@ export default function Home() {
       {active === lists[1] && <CoughAndCold />}
       {active === lists[2] && <Vitamins />}
       <Link
-        href="/checkout"
-        className="bg-[#F6D211] w-fit p-4 rounded-full absolute bottom-2 right-2"
+        href={"/checkout"}
+        className="bg-[#F6D211] p-4 rounded-full sticky mt-2 bottom-2 w-fit self-end"
       >
-        <img src="/assets/images/cart.svg" alt="" />
+        <div className="relative">
+          <img src="/assets/images/cart.svg" alt="" />
+          <div className="absolute bg-white rounded-full w-4 h-4 flex items-center justify-center -top-4 -right-4">
+            <p className=" text-black text-[10px] text-center ">{product.length}</p>
+          </div>
+        </div>
       </Link>
     </main>
   );
