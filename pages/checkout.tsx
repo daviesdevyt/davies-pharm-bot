4217eee9cd2416b4c788d3a439c2a967b7168672
Temidire <temidireowoeye@gmail.com>
2024-08-05 19:48:12 +0100

    fix: made shipping address editable

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -2,11 +2,20 @@
 import { format } from "@/constants/drugs";
 import { useProductsStore } from "@/store/useProducts";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { ReactHTMLElement, useEffect, useState } from "react";
 
 const Checkout = () => {
   const { product, removeProduct } = useProductsStore();
   const [hydrated, setHydrated] = useState(false);
+
+  const [edit, setEdit] = useState<boolean>(true);
+
+  const [shippingDetails, setShippingDetails] = useState({
+    country: "Nile",
+    house:
+      "Plot 681, Cadastral Zone C, OO, Research & Institution Area,Airport Road, Jabi",
+    phone: "+2349073210998",
+  });
 
   useEffect(() => {
     setHydrated(true);
@@ -16,9 +25,17 @@ const Checkout = () => {
     return null; // or a loading indicator
   }
 
-//   const getTotal = (products: {price: number}[]) => {
-//     products.reduce((accumulator, item) => (accumulator + item.price) )
-//   }
+  //   const getTotal = (products: {price: number}[]) => {
+  //     products.reduce((accumulator, item) => (accumulator + item.price) )
+  //   }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setShippingDetails((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
   return (
     <main className="min-h-screen space-y-5 p-5 text-white">
@@ -37,7 +54,11 @@ const Checkout = () => {
               key={i}
             >
               <div className="flex space-x-3">
-                <img src={items.image} className=" h-[45px] w-[80px]" alt="Image" />
+                <img
+                  src={items.image}
+                  className=" h-[45px] w-[80px]"
+                  alt="Image"
+                />
                 <div>
                   <h1 className="font-medium ">{items.name}</h1>
                   <h2 className="text-[15px] font-bold text-[#F6D211]">
@@ -78,15 +99,35 @@ const Checkout = () => {
           <section className="space-y-2">
             <header className="flex items-center justify-between">
               <h1 className="text-[18px] font-bold">Shipping Address</h1>
-              <p className="text-[#F6D211] cursor-pointer">change</p>
+              <p
+                className="text-[#F6D211] cursor-pointer"
+                onClick={() => setEdit(!edit)}
+              >
+                {!edit ? "save" : "change"}
+              </p>
             </header>
             <div className="flex flex-col space-y-2 rounded-[20px] bg-black p-[10px]">
-              <p className="border-b text-[12.09px]">Nile</p>
-              <p className="border-b text-[10.57px]">
-                Plot 681, Cadastral Zone C, OO, Research & Institution Area,
-                Airport Road, Jabi
-              </p>
-              <p className="text-[10.57px]">+234 9077373738</p>
+              <input
+                name="country"
+                value={shippingDetails.country}
+                className="border-b text-[12.09px] outline-none bg-transparent"
+                readOnly={edit}
+                onChange={handleChange}
+              />
+              <input
+                className="border-b text-[10.57px] outline-none bg-transparent"
+                name="house"
+                value={shippingDetails.house}
+                readOnly={edit}
+                onChange={handleChange}
+              />
+              <input
+                name="phone"
+                className="text-[10.57px] outline-none bg-transparent"
+                value={shippingDetails.phone}
+                readOnly={edit}
+                onChange={handleChange}
+              />
             </div>
           </section>
           <section className="space-y-2">
@@ -99,7 +140,13 @@ const Checkout = () => {
           </section>
           <section className="flex justify-between">
             <p className="text-[17px]">Total</p>
-            <h1 className="font-medium">${product.reduce((accumulator, item) => (accumulator + item.price), 0)}</h1>
+            <h1 className="font-medium">
+              $
+              {product.reduce(
+                (accumulator, item) => accumulator + item.price,
+                0
+              )}
+            </h1>
           </section>
           <button className="w-full rounded-[30px] bg-[#F6D211] px-[70px] py-[19px] text-[17px] text-black">
             Proceed to payment
